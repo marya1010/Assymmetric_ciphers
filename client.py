123ea3fe7a75f65ae89284dca48de91eaf15fe16
marya1010 <marya_mirmanova@mail.ru>
2022-12-06 23:00:00 +0300

    Получаем число К

--- client.py
+++ client.py
@@ -1,14 +1,31 @@
 import socket
-import pickle
+import random
 
-HOST = '127.0.0.1'
-PORT = 8080
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
-
-p, g, a = 7, 5, 3
+a = random.randint(10, 20)
+p, g = 7, 5
 A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+A_g_p = str(A)+str(g)+str(p)#[A, g, p]
+print(A_g_p)
+print(client.recv(1024).decode('utf-8'))
 
-sock.close()
+print('посылаю Агп на сервер...')
+client.send(A_g_p.encode('utf-8'))
+
+print("получаю B...")
+B = client.recv(1024).decode('utf-8')
+
+K = int(B)**a % p
+print('Полученное число К: ', K)
+
+while True:
+
+    data = client.recv(1024)
+    print(data.decode('utf-8'))
+    print('**Введите сообщение для сервера:** ')
+    message = input().encode('utf-8')
+    client.send(message)
+    if message == 'exit':
+        break
--- server.py
+++ server.py
@@ -1,15 +1,39 @@
 import socket
 import pickle
+import random
+import time
 
-HOST = '127.0.0.1'
-PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Host = '127.0.0.1'
+Port = 55555
+server.bind((Host, Port))
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+server.listen()
+user, address = server.accept()
+user.send('начинаем работу!'.encode('utf-8'))
+print("получаю Агп...")
+A_g_p = user.recv(1024).decode('utf-8')
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+A = int(A_g_p[0])
+g = int(A_g_p[1])
+p = int(A_g_p[2])
 
-conn.close()
+print('генерирую В...')
+b = random.randint(10, 20)
+B = g ** b % p
+
+print('отправляю В...')
+user.send(str(B).encode('utf-8'))
+
+K = A**b % p
+print('Полученное число К: ', K)
+
+while True:
+
+    print('**Введите сообщение для клиента:** ')
+    #user.send('Вы подключены'.encode('utf-8'))
+    user.send(input().encode('utf-8'))
+    data = user.recv(1024)
+    print(data.decode('utf-8'))
+    if data.decode('utf-8') == 'exit':
+        break
